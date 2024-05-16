ccc279ef28970b0bd361c880fba45a6b41c7cad0
nastysha15 <nast151007@gmail.com>
2024-05-16 12:12:22 +0300

    MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,4 +1,6 @@
-package com.example.myapplication
+package com.example.myapplication //Определяет пакет для класса MainActivity.
+// Необходимые импорты для функциональности Activity.
+
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -6,7 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // Определяет класс MainActivity как подкласс AppCompatActivity, что делает его компонентом пользовательского интерфейса.
+// Входной параметр: savedInstanceState: Объект Bundle, содержащий сохраненное состояние Activity.
+// Действия:
+    // Вызывает метод super.onCreate() для инициализации Activity.
+    // Устанавливает макет activity_main.xml в качестве содержимого Activity.
+    //Проверяет, был ли ранее сохранен экземпляр Activity (savedInstanceState не равен null). Если нет, то:
+        //Создает новый фрагмент MainFragment с помощью MainFragment().
+        // Добавляет фрагмент в контейнер R.id.container с помощью supportFragmentManager.beginTransaction().
+        // Выполняет транзакцию немедленно с помощью commitNow().
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,3 +26,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+//
